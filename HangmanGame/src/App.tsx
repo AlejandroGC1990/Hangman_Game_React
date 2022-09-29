7f28cc943a9780fb21ef1b7209bdd80002baac7e
AlexOscar <alejandro.garcia.carmona@gmail.com>
2022-09-29 03:21:37 +0200

    message if won or lose

--- HangmanGame/src/App.tsx
+++ HangmanGame/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { HangImage } from './components/HangImage';
 import { letters } from './helpers/letters';
 
 function App() {
   
-  const [word] = useState('COMPUTADORA');
+  const [word] = useState('PATATA');
   const [hiddenWord, setHiddenWord] = useState('_ '.repeat(word.length));
-  const [ attempts, setAttempts ] = useState(0);
+  const [attempts, setAttempts ] = useState(0);
+  const [lose, setLose] = useState(false);
+  const [won, setWon] = useState(false);
+
+  useEffect(() => {
+    if(attempts >= 9){
+      setLose(true);
+    }
+  },[attempts]);
+
+  useEffect(() => {
+    const currentHiddenWord = hiddenWord.split(' ').join('');
+
+    if(currentHiddenWord === word){
+      setWon(true);
+    }
+  },[hiddenWord]);
 
   const checkLetter = ( letter:string ) => {
+    if(lose) return;
+    
     if(!word.includes(letter)){
       setAttempts(Math.min(attempts + 1, 9));
       return;
@@ -34,8 +52,18 @@ function App() {
       {/*Hidden word*/}
       <h3>{hiddenWord}</h3>
       
-      {/*Intentos*/}
+      {/*Attempts count*/}
       <h3>Intentos: {attempts}</h3>
+
+      {/*Message - Lose*/}
+      {
+        (lose) ? <h2>Lose {word}</h2>: ''
+      }
+
+      {/*Message - Win*/}
+      {
+        (won) ? <h2>Winner</h2>: ''
+      }
 
       {/*Buttons for letters and control failed attempts*/}
       {
